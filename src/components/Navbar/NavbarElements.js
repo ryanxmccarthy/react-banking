47f459cdb60043349554d2d5eb39b2730af86337
Ryan XVX <ryanxmccarthy@gmail.com>
2022-02-09 12:38:48 -0800

    further styling logo

--- src/components/Navbar/NavbarElements.js
+++ src/components/Navbar/NavbarElements.js
@@ -30,5 +30,13 @@ export const NavbarContainer = styled.div`
 `
 
 export const NavLogo = styled(LinkR)`
-    
+    color: #fff;
+    justify-self: flex-start;
+    cursor: pointer;
+    font-size: 1.5rem;
+    display: flex;
+    align-items: center;
+    margin-left: 24px;
+    font-weight: bold;
+    text-decoration: none;
 `
