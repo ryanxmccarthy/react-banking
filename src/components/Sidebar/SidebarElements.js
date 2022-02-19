f5d4b6d865a2ca36416922a285c201bf52cf3df0
Ryan XVX <ryanxmccarthy@gmail.com>
2022-02-19 15:51:31 -0800

    styling icons

--- src/components/Sidebar/SidebarElements.js
+++ src/components/Sidebar/SidebarElements.js
@@ -15,3 +15,16 @@ export const SidebarContainer = styled.aside`
     opacity: ${({ isOpen }) => (isOpen ? '100%' : '0')};
     top: ${({ isOpen }) => (isOpen ? '0' : '-100%')};
 `
+export const CloseIcon = styled(FaTimes)`
+    color: #fff;
+`
+
+export const Icon = styled.div`
+    position: absolute;
+    top: 1.2rem;
+    right: 1.5rem;
+    background: transparent;
+    font-size: 2rem;
+    cursor: pointer;
+    outline: none;
+`
