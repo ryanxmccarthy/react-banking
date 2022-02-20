3373f379c2200f8aab1db80edb4fc0ecaf7d3b64
Ryan XVX <ryanxmccarthy@gmail.com>
2022-02-20 10:38:03 -0800

    styling sidebar route

--- src/components/Sidebar/SidebarElements.js
+++ src/components/Sidebar/SidebarElements.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components'
+import { Link as LinkS } from 'react-scroll'
 import {FaTimes} from 'react-icons/fa'
 
 export const SidebarContainer = styled.aside`
@@ -54,4 +55,24 @@ export const SidebarLink = styled(LinkS)`
 export const SideBtnWrap = styled.div`
     display: flex;
     justify-content: center;
+`
+
+export const SidebarRoute = styled(LinkR)`
+    border-radius: 50px;
+    background: #01bf71;
+    white-space: nowrap;
+    padding: 16px 64px;
+    color: #010606;
+    font-size: 16px;
+    outline: none;
+    border: none;
+    cursor: pointer;
+    transition: all 0.2s ease-in-out;
+    text-decoration: none;
+
+    &:hover {
+        transition: all 0.2s ease-in-out;
+        background: #fff;
+        color: #010606
+    }
 `
