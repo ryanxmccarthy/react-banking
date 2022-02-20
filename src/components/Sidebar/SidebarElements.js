bdc4b03a4ba4abec347e7faba0b926509421b091
Ryan XVX <ryanxmccarthy@gmail.com>
2022-02-20 10:42:39 -0800

    further styling sidebar

--- src/components/Sidebar/SidebarElements.js
+++ src/components/Sidebar/SidebarElements.js
@@ -35,6 +35,17 @@ export const SidebarWrapper = styled.div`
     color: #fff;
 `
 
+export const SidebarMenu = styled.ul`
+    display: grid;
+    grid-template-column: 1fr;
+    grid-template-rows: repeat(6, 80px);
+    text-align: center;
+
+    @media screen and (max-width: 480px) {
+        grid-template-rows: repeat(6, 60px);
+    }
+`
+
 export const SidebarLink = styled(LinkS)`
     display: flex;
     align-items: center;
