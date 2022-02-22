093546134f5a05f0f3deb79a5ed471646619cac4
Ryan XVX <ryanxmccarthy@gmail.com>
2022-02-22 09:45:27 -0800

    adding content to hero

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -2,7 +2,11 @@ import React from 'react'
 
 const HeroSection = () => {
   return (
-    <HeroContainer id='home'></HeroContainer>
+    <HeroContainer>
+        <HeroBg>
+            <VideoBg autoPlay loop muted src={Video} type='/video/mp4' />
+        </HeroBg>
+    </HeroContainer>
   )
 }
 
