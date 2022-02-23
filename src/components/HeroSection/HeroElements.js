56bd44d44a0377e77bce504f33ef33f8c13a90d6
Ryan XVX <ryanxmccarthy@gmail.com>
2022-02-23 09:24:19 -0800

    styling hero background

--- src/components/HeroSection/HeroElements.js
+++ src/components/HeroSection/HeroElements.js
@@ -9,4 +9,17 @@ export const HeroContainer = styled.div`
     height: 800px;
     position: relative;
     z-index: 1;
+
+    // Add :before styles
+`
+
+export const HeroBg = styled.div`
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
 `
