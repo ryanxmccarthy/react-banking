5b4bbe4316fa52c4ade11ae6c43f87f492cd49ad
Ryan XVX <ryanxmccarthy@gmail.com>
2022-02-27 17:27:14 -0800

    adding hover for button

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,8 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Video from '../../videos/video.mp4'
-import { HeroContainer, HeroBg, VideoBg } from './HeroElements'
+import { HeroContainer, HeroBg, VideoBg, HeroContent, HeroH1, HeroP, HeroBtnWrapper, ArrowForward, ArrowRight } from './HeroElements'
 
 const HeroSection = () => {
+  const [hover, setHover] = useState(false)
+
+  
+
   return (
     <HeroContainer>
         <HeroBg>
