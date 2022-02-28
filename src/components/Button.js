3e0d477b99d6b766d5f611119632283cdf5d5c89
Ryan XVX <ryanxmccarthy@gmail.com>
2022-02-27 17:39:49 -0800

    dynamic button styling

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,5 +3,9 @@ import { Link } from 'react-scroll'
 
 export const Button = styled(Link)`
     border-radius: 50px;
-    background: 
+    background: ${({primary}) => (primary ? '#01BF71' : '#010606')};
+    white-space: nowrap;
+    padding: ${({big}) => (big ? '14px 48px' : ' 12px 30px')}
+    color: ${({dark}) => (dark ? '#010606' : '#fff')}
+    font-size: ${({fontBig}) => (fontBig ? '20px' : '16px')}
 `
