54d5470c1fc5c9371ced20ca745a70f2b89024d0
Ryan XVX <ryanxmccarthy@gmail.com>
2022-02-28 16:07:09 -0800

    further button styling

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import Video from '../../videos/video.mp4'
 import { HeroContainer, HeroBg, VideoBg, HeroContent, HeroH1, HeroP, HeroBtnWrapper, ArrowForward, ArrowRight } from './HeroElements'
+import { Button } from '../Button.js'
 
 const HeroSection = () => {
   const [hover, setHover] = useState(false)
