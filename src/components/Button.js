c3cd349c9ded15c488531bc4f5618fae8c294883
Ryan XVX <ryanxmccarthy@gmail.com>
2022-02-28 16:07:18 -0800

    further button styling

--- src/components/Button.js
+++ src/components/Button.js
@@ -8,4 +8,13 @@ export const Button = styled(Link)`
     padding: ${({big}) => (big ? '14px 48px' : ' 12px 30px')}
     color: ${({dark}) => (dark ? '#010606' : '#fff')}
     font-size: ${({fontBig}) => (fontBig ? '20px' : '16px')}
+    outline: none;
+    border: none:
+    cursor: pointer;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    transition: all 0.2s ease-in-out;
+
+    
 `
