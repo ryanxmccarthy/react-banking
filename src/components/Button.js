5ff1bc3c260fbf2d53b8f8d9b0fe7fe4513f238a
Ryan XVX <ryanxmccarthy@gmail.com>
2022-02-28 16:09:57 -0800

    fixing style display issues

--- src/components/Button.js
+++ src/components/Button.js
@@ -5,11 +5,11 @@ export const Button = styled(Link)`
     border-radius: 50px;
     background: ${({primary}) => (primary ? '#01BF71' : '#010606')};
     white-space: nowrap;
-    padding: ${({big}) => (big ? '14px 48px' : ' 12px 30px')}
-    color: ${({dark}) => (dark ? '#010606' : '#fff')}
-    font-size: ${({fontBig}) => (fontBig ? '20px' : '16px')}
+    padding: ${({big}) => (big ? '14px 48px' : ' 12px 30px')};
+    color: ${({dark}) => (dark ? '#010606' : '#fff')};
+    font-size: ${({fontBig}) => (fontBig ? '20px' : '16px')};
     outline: none;
-    border: none:
+    border: none;
     cursor: pointer;
     display: flex;
     justify-content: center;
