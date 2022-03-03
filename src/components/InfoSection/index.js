5bf5fe0a15ee1802c0a9df8270014cde6a7d3eee
Ryan XVX <ryanxmccarthy@gmail.com>
2022-03-03 15:30:28 -0800

    structuring info section

--- src/components/InfoSection/index.js
+++ src/components/InfoSection/index.js
@@ -1,8 +1,26 @@
 import React from 'react'
+import { Button } from 'react-scroll'
 
 const InfoSection = () => {
   return (
-    <div>InfoSection</div>
+    <>
+        <InfoContainer id={id}>
+            <InfoWrapper>
+                <InfoRow>
+                    <Column1>
+                        <TextWrapper>
+                            <TopLine>TopLine</TopLine>
+                            <Heading>Heading</Heading>
+                            <Subtitle>Subtitle</Subtitle>
+                            <BtnWrap>
+                                <Button to='home'>Button</Button>
+                            </BtnWrap>
+                        </TextWrapper>
+                    </Column1>
+                </InfoRow>   
+            </InfoWrapper> 
+        </InfoContainer>
+    </>
   )
 }
 
