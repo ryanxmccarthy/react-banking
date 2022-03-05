850279301416574fb80da61273de4cb155d6ce21
Ryan XVX <ryanxmccarthy@gmail.com>
2022-03-05 12:48:06 -0800

    styling info row

--- src/components/InfoSection/InfoElements.js
+++ src/components/InfoSection/InfoElements.js
@@ -20,3 +20,17 @@ export const InfoWrapper = styled.div`
     padding: 0 24px;
     justify-content: center;
 `
+
+export const InfoRow = styled.div`
+    display: grid;
+    grid-auto-columns: minmax(auto, 1fr);
+    align-items: center;
+    grid-template-areas: ${({imgStart}) => (imgStart ? `'col2 col1'` : `'col1 col2'`)}
+
+    @media screen and (max-width: 768px) {
+        grid-template-areas: ${({imgStart}) => (imgStart ? `'col1' 'col2'` : `'col1 col2'`)}
+
+    }
+`
+
+
