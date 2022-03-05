1d9182efbe81b52113b30cb241baa51f27f93b71
Ryan XVX <ryanxmccarthy@gmail.com>
2022-03-05 12:24:15 -0800

    styling info wrapper

--- src/components/InfoSection/InfoElements.js
+++ src/components/InfoSection/InfoElements.js
@@ -8,3 +8,15 @@ export const InfoContainer = styled.div`
         padding: 100px 0;
     }
 `
+
+export const InfoWrapper = styled.div`
+    display: grid;
+    z-index: 1;
+    height: 860px;
+    width: 100%;
+    max-width: 1100px;
+    margin-right: auto;
+    margin-left: auto;
+    padding: 0 24px;
+    justify-content: center;
+`
