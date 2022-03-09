b19265dd13268a5e70b08d36669d6b58e616db05
Ryan XVX <ryanxmccarthy@gmail.com>
2022-03-08 17:19:30 -0800

    styling image

--- src/components/InfoSection/InfoElements.js
+++ src/components/InfoSection/InfoElements.js
@@ -88,3 +88,9 @@ export const ImgWrap = styled.div`
     max-width: 555px;
     height: 100%;
 `
+
+export const Img = styled.img`
+    width: 100%;
+    margin: 0 0 10px 0;
+    padding-right: 0;
+`
