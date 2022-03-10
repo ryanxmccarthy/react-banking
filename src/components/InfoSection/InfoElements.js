50af2b5ffe3bb52dc16e2eff82846d70ccd12dcb
Ryan XVX <ryanxmccarthy@gmail.com>
2022-03-09 20:45:02 -0800

    fixing css error

--- src/components/InfoSection/InfoElements.js
+++ src/components/InfoSection/InfoElements.js
@@ -62,7 +62,7 @@ export const TopLine = styled.p`
 export const Heading = styled.h1`
     margin-bottom: 24px;
     font-size: 48px;
-    line-height: 1.1:
+    line-height: 1.1;
     font-weight: 600;
     color: ${({ lightText }) => (lightText ? '#f7f8fa' : '010606')};
 
