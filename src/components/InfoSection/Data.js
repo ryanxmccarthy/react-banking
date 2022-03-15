cc1545088486f7db21e9dd00447af285deccaf9e
Ryan XVX <ryanxmccarthy@gmail.com>
2022-03-15 14:52:09 -0700

    fixing css issues for first card

--- src/components/InfoSection/Data.js
+++ src/components/InfoSection/Data.js
@@ -8,7 +8,7 @@ export const homeObjOne = {
     description: 'Get access to our exclusive app that allows you to send unlimited transacations without getting charged any fees.',
     buttonLabel: 'Get started',
     imgStart: false,
-    img: require('../../images/svg-1.svg'),
+    img: require('../../images/phonepicutres-TA.png'),
     alt: 'Car',
     dark: true,
     primary: true,
