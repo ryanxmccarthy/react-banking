6be4dad9a2e703c2ac6e1a5a3f0573bda14d8ed8
Ryan XVX <ryanxmccarthy@gmail.com>
2022-03-16 13:26:33 -0700

    adding new home objects

--- src/components/InfoSection/Data.js
+++ src/components/InfoSection/Data.js
@@ -14,3 +14,37 @@ export const homeObjOne = {
     dark: true,
     primary: true
 }
+
+export const homeObjTwo = {
+    id: 'about',
+    lightBg: false,
+    lightText: true,
+    darkText: false,
+    lightTextDesc: true,
+    topLine: 'Premium Bank',
+    headline: 'Unlimited Transactions with zero fees',
+    description: 'Get access to our exclusive app that allows you to send unlimited transacations without getting charged any fees.',
+    buttonLabel: 'Get started',
+    imgStart: false,
+    img: require('../../images/phonepicutres-TA.png'),
+    alt: 'Car',
+    dark: true,
+    primary: true
+}
+
+export const homeObjThree = {
+    id: 'about',
+    lightBg: false,
+    lightText: true,
+    darkText: false,
+    lightTextDesc: true,
+    topLine: 'Premium Bank',
+    headline: 'Unlimited Transactions with zero fees',
+    description: 'Get access to our exclusive app that allows you to send unlimited transacations without getting charged any fees.',
+    buttonLabel: 'Get started',
+    imgStart: false,
+    img: require('../../images/phonepicutres-TA.png'),
+    alt: 'Car',
+    dark: true,
+    primary: true
+}
