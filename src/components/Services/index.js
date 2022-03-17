355bd175b34fbeba15453257b439c1577aa9af38
Ryan XVX <ryanxmccarthy@gmail.com>
2022-03-17 14:51:35 -0700

    adding services cards to correct file

---
+++ src/components/Services/index.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Services = () => {
+  return (
+    <ServicesContainer id='services'>
+        <ServicesH1>Our Services</ServicesH1>
+        <ServicesWrapper>
+            <ServicesCard>
+                <ServicesIcon src={Icon1} />
+                <ServicesH2>Reduce expenses</ServicesH2>
+                <ServicesP>We help reduce your fees and increase you overall revenue.</ServicesP>
+            </ServicesCard>
+            <ServicesCard>
+                <ServicesIcon src={Icon2} />
+                <ServicesH2>Virtual offices</ServicesH2>
+                <ServicesP>Access our platform anywhere in the world.</ServicesP>
+            </ServicesCard>
+            <ServicesCard>
+                <ServicesIcon src={Icon3} />
+                <ServicesH2>Premium benefits</ServicesH2>
+                <ServicesP>Unlock special membership card.</ServicesP>
+            </ServicesCard>
+        </ServicesWrapper>
+    </ServicesContainer>
+  )
+}
+
+export default Services
