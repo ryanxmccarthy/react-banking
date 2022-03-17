b09097a3005fe5cf37373dd1f84d618e6f6284f3
Ryan XVX <ryanxmccarthy@gmail.com>
2022-03-17 14:50:26 -0700

    adding copy to services cards

--- src/components/Services/ServicesElements.js
+++ src/components/Services/ServicesElements.js
@@ -12,13 +12,13 @@ const Services = () => {
             </ServicesCard>
             <ServicesCard>
                 <ServicesIcon src={Icon2} />
-                <ServicesH2>Reduce expenses</ServicesH2>
-                <ServicesP>We help reduce your fees and increase you overall revenue.</ServicesP>
+                <ServicesH2>Virtual offices</ServicesH2>
+                <ServicesP>Access our platform anywhere in the world.</ServicesP>
             </ServicesCard>
             <ServicesCard>
                 <ServicesIcon src={Icon3} />
-                <ServicesH2>Reduce expenses</ServicesH2>
-                <ServicesP>We help reduce your fees and increase you overall revenue.</ServicesP>
+                <ServicesH2>Premium benefits</ServicesH2>
+                <ServicesP>Unlock special membership card.</ServicesP>
             </ServicesCard>
         </ServicesWrapper>
     </ServicesContainer>
