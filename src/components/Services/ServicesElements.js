b2330fb01d42e525ac8593bc2f67c01d0163a6de
Ryan XVX <ryanxmccarthy@gmail.com>
2022-03-17 14:20:17 -0700

    creating strucutre of services card

---
+++ src/components/Services/ServicesElements.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Services = () => {
+  return (
+    <ServicesContainer id='services'>
+        <ServicesH1>Our Services</ServicesH1>
+        <ServicesWrapper>
+            <ServicesCard>
+                <ServicesIcon src={Icon1} />
+                <ServicesH2>Reduce expenses</ServicesH2>
+                <ServicesP>We help reduce your fees and increase you overall revenue.</ServicesP>
+            </ServicesCard>
+        </ServicesWrapper>
+    </ServicesContainer>
+  )
+}
+
+export default Services
