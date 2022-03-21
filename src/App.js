c3b1a70fdb44802c11c3547d2625ea23b951bfe4
Ryan XVX <ryanxmccarthy@gmail.com>
2022-03-21 09:30:36 -0700

    working with updated router docs

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages'
 import SignInPage from './pages/signIn';
 
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route path='/' component={Home} exact />
+      <Routes>
+        <Route path='/' index element={<Home />}  />
         <Route path='/signin' component={SignInPage} exact />
-      </Switch>
+      </Routes>
     </Router>
   );
 }
