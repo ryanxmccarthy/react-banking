46cf89209e0f7c6aac0697115f3c1272a0cb4d24
Ryan XVX <ryanxmccarthy@gmail.com>
2022-03-21 09:39:42 -0700

    adding navbar to sign in page

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages'
 import SignInPage from './pages/signIn';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path='/' index element={<Home />}  />
         <Route path='/signin' element={<SignInPage />} />
