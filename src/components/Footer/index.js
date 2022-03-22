5afa610666125d7c586440a9a4eaed93860abc56
Ryan XVX <ryanxmccarthy@gmail.com>
2022-03-22 09:36:33 -0700

    adding link to footer item

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,8 +2,20 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div></div>
-  )
+    <FooterContainer>
+        <FooterWrap>
+            <FooterLinksContainer>
+                <FooterLinksWrapper>
+                    <FooterLinkItems>
+                        <FooterLinkTitle>
+                            <FooterLink to='/signin'>About us</FooterLink>
+                        </FooterLinkTitle>
+                    </FooterLinkItems>
+                </FooterLinksWrapper>
+            </FooterLinksContainer>
+        </FooterWrap>
+    </FooterContainer>  
+)
 }
 
 export default Footer
