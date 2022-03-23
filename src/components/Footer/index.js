72bd286d102cb72fe0d8c3c95f7f9072e45d9250
Ryan XVX <ryanxmccarthy@gmail.com>
2022-03-23 12:16:36 -0700

    adding footer title

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -7,15 +7,13 @@ const Footer = () => {
             <FooterLinksContainer>
                 <FooterLinksWrapper>
                     <FooterLinkItems>
-                        <FooterLinkTitle>
-                            <FooterLink to='/signin'>About us</FooterLink>
-                            <FooterLink to='/signin'>How it works</FooterLink>
-                            <FooterLink to='/signin'>Testimonials</FooterLink>
-                            <FooterLink to='/signin'>Careers</FooterLink>
-                            <FooterLink to='/signin'>Investors</FooterLink>
-                            <FooterLink to='/signin'>Terms of Service</FooterLink>
-                        </FooterLinkTitle>
-                    </FooterLinkItems>
+                        <FooterLinkTitle>About Us</FooterLinkTitle>
+                        <FooterLink to='/signin'>How it works</FooterLink>
+                        <FooterLink to='/signin'>Testimonials</FooterLink>
+                        <FooterLink to='/signin'>Careers</FooterLink>
+                        <FooterLink to='/signin'>Investors</FooterLink>
+                        <FooterLink to='/signin'>Terms of Service</FooterLink>
+                    </FooterLinkItems> 
                 </FooterLinksWrapper>
             </FooterLinksContainer>
         </FooterWrap>
