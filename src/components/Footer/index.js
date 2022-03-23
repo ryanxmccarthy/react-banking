c711ba2dd31ecd81d82bbcad5494a2e049be4923
Ryan XVX <ryanxmccarthy@gmail.com>
2022-03-23 12:13:38 -0700

    changing footer links

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -9,11 +9,11 @@ const Footer = () => {
                     <FooterLinkItems>
                         <FooterLinkTitle>
                             <FooterLink to='/signin'>About us</FooterLink>
-                            <FooterLink to='/signin'>About us</FooterLink>
-                            <FooterLink to='/signin'>About us</FooterLink>
-                            <FooterLink to='/signin'>About us</FooterLink>
-                            <FooterLink to='/signin'>About us</FooterLink>
-                            <FooterLink to='/signin'>About us</FooterLink>
+                            <FooterLink to='/signin'>How it works</FooterLink>
+                            <FooterLink to='/signin'>Testimonials</FooterLink>
+                            <FooterLink to='/signin'>Careers</FooterLink>
+                            <FooterLink to='/signin'>Investors</FooterLink>
+                            <FooterLink to='/signin'>Terms of Service</FooterLink>
                         </FooterLinkTitle>
                     </FooterLinkItems>
                 </FooterLinksWrapper>
